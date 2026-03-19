5511f54cb41e42ce3b4239dec24f4813235ee3f1
Cleyton-Alberto <zeusg2689@gmail.com>
2026-03-19 15:43:21 -0300

    added github button

--- scripts/see_more.js
+++ scripts/see_more.js
@@ -6,10 +6,10 @@ toggleButton.forEach(button => {
 
         if (moreText.style.display === "block") {
             moreText.style.display = "none";
-            button.textContent = "Ver mais...";
+            button.textContent = "▶Ver mais...";
         } else {
             moreText.style.display = "block";
-            button.textContent = "Ver menos";
+            button.textContent = "▶Ver menos";
         }
     });
 });
