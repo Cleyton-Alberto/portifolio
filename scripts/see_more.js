3c00405a58a78894348b6a6322783adc423b0929
Cleyton-Alberto <zeusg2689@gmail.com>
2026-03-19 15:23:26 -0300

    added see more button

---
+++ scripts/see_more.js
@@ -0,0 +1,15 @@
+const toggleButton = document.querySelectorAll(".toggleButton");
+
+toggleButton.forEach(button => {
+    button.addEventListener("click", () => {
+        const moreText = button.previousElementSibling;
+
+        if (moreText.style.display === "block") {
+            moreText.style.display = "none";
+            button.textContent = "Ver mais...";
+        } else {
+            moreText.style.display = "block";
+            button.textContent = "Ver menos";
+        }
+    });
+});
