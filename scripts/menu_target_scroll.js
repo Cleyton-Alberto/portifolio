b78d3eef2f6da7d71a7cc57088d38e7264ea8532
Cleyton-Alberto <zeusg2689@gmail.com>
2026-03-23 14:00:10 -0300

    Menu Fixed

---
+++ scripts/menu_target_scroll.js
@@ -0,0 +1,18 @@
+const navLinks = document.querySelectorAll("#menuOptions .link a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            const headerHeight = document.querySelector("header").offsetHeight;
+            const targetPosition = target.offsetTop - headerHeight - 20;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
